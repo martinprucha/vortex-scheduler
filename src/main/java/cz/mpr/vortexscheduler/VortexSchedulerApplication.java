3909fad8c7ef6afadb20657a8b7458234d11db71
Martin Průcha <prucha.martin@gmail.com>
2022-04-27 14:56:19 +0200

    Initial commit.

---
+++ src/main/java/cz/mpr/vortexscheduler/VortexSchedulerApplication.java
@@ -0,0 +1,13 @@
+package cz.mpr.vortexscheduler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VortexSchedulerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VortexSchedulerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cz/mpr/vortexscheduler/VortexSchedulerApplicationTests.java
@@ -0,0 +1,13 @@
+package cz.mpr.vortexscheduler;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VortexSchedulerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
